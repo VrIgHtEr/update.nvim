788e544f2e4d08b5b6bb6c1369962af97ae1e2ed
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-01-21 13:16:35 +0100

    updated to build in release mode

--- lua/update-nvim/init.lua
+++ lua/update-nvim/init.lua
@@ -1,24 +1,24 @@
 local M = {}
-local env = require("toolshed.env")
-local a = require("toolshed.async")
-local dirname = "neovim"
+local env = require 'toolshed.env'
+local a = require 'toolshed.async'
+local dirname = 'neovim'
 
 function M.setup()
-	a.run(function()
-		a.spawn_a({ "ln", "-s", env.bin .. "/nvim", vim.fn.fnamemodify("~", ":p") .. "/.local/bin/nvim" })
-		env.install_dependencies({
-			{
-				dirname = dirname,
-				repo = "https://github.com/neovim/neovim",
-				builddeps = {},
-				buildcmd = {
-					{ "make", "CMAKE_EXTRA_FLAGS=-DCMAKE_INSTALL_PREFIX=" .. env.root },
-					{ "make", "install" },
-					{ "rm", "-rf", "build/" },
-				},
-			},
-		})
-	end)
+    a.run(function()
+        a.spawn_a { 'ln', '-s', env.bin .. '/nvim', vim.fn.fnamemodify('~', ':p') .. '/.local/bin/nvim' }
+        env.install_dependencies {
+            {
+                dirname = dirname,
+                repo = 'https://github.com/neovim/neovim',
+                builddeps = {},
+                buildcmd = {
+                    { 'make', 'CMAKE_BUILD_TYPE=Release', 'CMAKE_EXTRA_FLAGS=-DCMAKE_INSTALL_PREFIX=' .. env.root },
+                    { 'make', 'install' },
+                    { 'rm', '-rf', 'build/' },
+                },
+            },
+        }
+    end)
 end
 
 return M
