e299d29da80e58026b546ce15ad69e9ebc99c739
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-01-21 14:55:16 +0100

    added keymap to update neovim

--- plugtool_cfg.lua
+++ plugtool_cfg.lua
@@ -3,5 +3,6 @@ return {
     after = { 'vrighter/toolshed.nvim' },
     config = function()
         require('update-nvim').setup()
+        nnoremap('<leader>pn', ':lua require"update-nvim".update()', 'silent', 'Update neovim')
     end,
 }
