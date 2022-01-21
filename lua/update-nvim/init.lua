4b6298f798b0652e473010f3255e1356197d8c21
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-01-21 14:52:04 +0100

    added update code

--- lua/update-nvim/init.lua
+++ lua/update-nvim/init.lua
@@ -1,7 +1,15 @@
 local M = {}
 local env = require 'toolshed.env'
 local a = require 'toolshed.async'
+local git = require 'toolshed.git'
 local dirname = 'neovim'
+local marker = env.get_dependency_path(dirname) .. '/update.nvim'
+
+local build_commands = {
+    { 'make', 'CMAKE_BUILD_TYPE=Release', 'CMAKE_EXTRA_FLAGS=-DCMAKE_INSTALL_PREFIX=' .. env.root },
+    { 'make', 'install' },
+    { 'touch', marker },
+}
 
 function M.setup()
     a.run(function()
@@ -11,13 +19,36 @@ function M.setup()
                 dirname = dirname,
                 repo = 'https://github.com/neovim/neovim',
                 builddeps = {},
-                buildcmd = {
-                    { 'make', 'CMAKE_BUILD_TYPE=Release', 'CMAKE_EXTRA_FLAGS=-DCMAKE_INSTALL_PREFIX=' .. env.root },
-                    { 'make', 'install' },
-                },
+                buildcmd = build_commands,
             },
         }
     end)
 end
 
+local function rebuild()
+    for _, x in ipairs(build_commands) do
+        vim.notify('Executing command:\n' .. vim.inspect(x), 'info', { title = 'update.nvim' })
+        local ret = a.spawn_lines_a({}, print)
+        if ret ~= 0 then
+            vim.schedule(function()
+                vim.notify('An error occurred while executing command\n' .. vim.inspect(x), 'error', { title = 'update.nvim' })
+            end)
+            return false
+        end
+        return true
+    end
+end
+
+function M.update()
+    return a.run(function()
+        local updates = assert(git.update_a(env.get_dependency_path(dirname)))
+        if #updates ~= 0 then
+            env.deleteFileOrDir(marker)
+        end
+        if env.file_exists(marker) then
+            return false
+        end
+        return rebuild()
+    end)
+end
 return M
