aeb182cd76c0bf31e5a150e3fdd6ca4929da0077
Cedric Mamo <cedric.mamo.1@gov.mt>
2022-01-21 14:56:03 +0100

    added missing <cr> in mapping

--- plugtool_cfg.lua
+++ plugtool_cfg.lua
@@ -3,6 +3,6 @@ return {
     after = { 'vrighter/toolshed.nvim' },
     config = function()
         require('update-nvim').setup()
-        nnoremap('<leader>pn', ':lua require"update-nvim".update()', 'silent', 'Update neovim')
+        nnoremap('<leader>pn', ':lua require"update-nvim".update()<cr>', 'silent', 'Update neovim')
     end,
 }
