52ad77fe92fe8352599954833b1188d58d450950
Cedric Mamo <vrighter@gmail.com>
2022-01-26 01:48:29 +0100

    Revert "updated plugtool_cfg"
    This reverts commit 5a3e4d572502512f3af306dcdb8bc3e72e0cfe78.

--- plugtool_cfg.lua
+++ plugtool_cfg.lua
@@ -1,5 +1,6 @@
 return {
-    plugin_type = require('plugtool.constants').type.update,
+    needs = { 'vrighter/toolshed.nvim' },
+    after = { 'vrighter/toolshed.nvim' },
     config = function()
         require('update-nvim').setup()
         nnoremap('<leader>pn', ':lua require"update-nvim".update()<cr>', 'silent', 'Update neovim')
